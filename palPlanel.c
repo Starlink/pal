b08ec06cf924b32aaeda88c6f685c81cf437bc43
Graham Bell <g.bell@eaobservatory.org>
2025-07-18 16:40:58 -1000

    palPlanel: correct documentation of returned array
    This described "u" rather than "pv".  Since the function fills the
    return value array using palUe2pv, copied the description of "pv"
    from the documentation of that function.

--- palPlanel.c
+++ palPlanel.c
@@ -38,17 +38,8 @@
 *        mean anomaly or longitude (radians, JFORM=1,2 only)
 *     dm = double (Given)
 *        daily motion (radians, JFORM=1 only)
-*     u = double [13] (Returned)
-*        Universal orbital elements (Note 1)
-*            (0)  combined mass (M+m)
-*            (1)  total energy of the orbit (alpha)
-*            (2)  reference (osculating) epoch (t0)
-*          (3-5)  position at reference epoch (r0)
-*          (6-8)  velocity at reference epoch (v0)
-*            (9)  heliocentric distance at reference epoch
-*           (10)  r0.v0
-*           (11)  date (t)
-*           (12)  universal eccentric anomaly (psi) of date, approx
+*     pv = double [6] (Returned)
+*       Position (AU) and velocity (AU/s)
 *     jstat = int * (Returned)
 *        status:  0 = OK
 *              - -1 = illegal JFORM
